492d4b1aa5fa7875a0f14c84d8206c2757932607
Iasminanjos103 <iasminanjos103@gmail.com>
2024-12-03 10:04:47 -0300

    aula 2

---
+++ AULA 2 - IASMIN/aula02.py
@@ -0,0 +1,10 @@
+print("bom dia")
+print("a vitoria é insuportável")
+nome_usuario = "iasmin" 
+idade_usuario = 17 
+altura_usuario = 1.59
+maior_idade = False
+print("seja bem vindo", nome_usuario)
+print("sua idade é", idade_usuario, "anos")
+print("sua altura é", altura_usuario, "metros")
+print("voce é maior de idade?", maior_idade)
